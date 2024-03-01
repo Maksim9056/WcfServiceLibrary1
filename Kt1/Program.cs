bc010c015cde4abe24cef148d2dbb1577ae2888d
Maksim9056 <bobreczovm@inbox.ru>
2024-03-01 14:44:48 +0300

    правки

--- Kt1/Program.cs
+++ Kt1/Program.cs
@@ -18,60 +18,60 @@ namespace Kt1
                 host.Close();
             }
 
-            Service1 service1 = new Service1();
-            while (true)
-            {
-                Console.WriteLine("Print :");
-                Console.WriteLine("0 -List contrack ");
-                Console.WriteLine("1 -Add contrack ");
-                Console.WriteLine("2-Find contrack id");
-                Console.WriteLine("3-Close");
+            //Service1 service1 = new Service1();
+            //while (true)
+            //{
+            //    Console.WriteLine("Print :");
+            //    Console.WriteLine("0 -List contrack ");
+            //    Console.WriteLine("1 -Add contrack ");
+            //    Console.WriteLine("2-Find contrack id");
+            //    Console.WriteLine("3-Close");
 
-                int numbers = Convert.ToInt32(Console.ReadLine());
-                switch (numbers)
-                {
-                    case 0:
+            //    int numbers = Convert.ToInt32(Console.ReadLine());
+            //    switch (numbers)
+            //    {
+            //        case 0:
 
-                        var Select = service1.ListContrat();
-                        foreach (var item in Select)
-                        {
-                            Console.WriteLine($"{item.Id} {item.Name_Contrack}");
+            //            var Select = service1.ListContrat();
+            //            foreach (var item in Select)
+            //            {
+            //                Console.WriteLine($"{item.Id} {item.Name_Contrack}");
 
-                        }
-                        if (Select.Count > 0)
-                        {
-                            Console.WriteLine("Not contrack");
-                        }
+            //            }
+            //            if (Select.Count > 0)
+            //            {
+            //                Console.WriteLine("Not contrack");
+            //            }
 
-                        break;
 
-                    case 1:
-                        Console.WriteLine("Id indificate enter");
-                        int i = Convert.ToInt32(Console.ReadLine());
+            //            break;
 
-                        Console.WriteLine("Enter name Contrack");
-                        string name = Console.ReadLine();
-                        Contrack contrack = new Contrack(i, name);
-                        service1.AddGet(contrack);
-                        break;
+            //        case 1:
+            //            Console.WriteLine("Id indificate enter");
+            //            int i = Convert.ToInt32(Console.ReadLine());
 
-                    case 2:
-                        Console.WriteLine("Id  contrack  enter for find");
-                        i = Convert.ToInt32(Console.ReadLine());
+            //            Console.WriteLine("Enter name Contrack");
+            //            string name = Console.ReadLine();
+            //            Contrack contrack = new Contrack(i, name);
+            //            service1.AddGet(contrack);
+            //            break;
 
-                        service1.Push(i);
+            //        case 2:
+            //            Console.WriteLine("Id  contrack  enter for find");
+            //            i = Convert.ToInt32(Console.ReadLine());
 
-                        break;
-                    case 3:
-                        System.Environment.Exit(1);
+            //            service1.Push(i);
 
-                        break;
-                }
-                Console.Clear();
+            //            break;
+            //        case 3:
+            //            System.Environment.Exit(1);
 
-            }
+            //            break;
+            //    }
+
+        }
 
 
         }
     }
-}
+
